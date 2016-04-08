3d14eb756fdcd8b074e7b39b34b4a07715c8d4fb
Michael J Hewitt <matroxmike@icloud.com>
2016-04-09 00:37:08 +1000

    added MyApp - from Oracle - V simple

---
+++ MyApp.java
@@ -0,0 +1,42 @@
+/*
+ * MyApp.java
+ * https://docs.oracle.com/javase/8/javafx/api/javafx/application/Application.html
+ * Copyright 2016 mike <mikeh@mikeh-OptiPlex-745>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+    public void start(Stage stage) {
+        Circle circ = new Circle(40, 40, 30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("My JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
